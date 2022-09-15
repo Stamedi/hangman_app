4a351427356c68e943dae613577e0cf7fa03290b
Mario Arro <marioarro186@gmail.com>
2022-09-15 12:53:47 +0300

    visuals

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -12,55 +12,55 @@ function Figure({attempts}) {
 
       {/* <!-- Head --> */}
       {attempts < 6 &&
-        <circle cx="200" cy="80" r="30" />
+        <circle cx="200" cy="80" r="30" className='head' />
       }
       {/* <!-- Body --> */}
       {attempts < 5 &&
-        <line x1="200" y1="110" x2="200" y2="190" />
+        <line x1="200" y1="110" x2="200" y2="190" className='body' />
       }
       {/* <!-- Arms --> */}
       {attempts < 4 &&
-        <line x1="200" y1="140" x2="150" y2="110" />
+        <line x1="200" y1="140" x2="150" y2="110" className='arm1' />
       }
       {attempts < 3 &&
-        <line x1="200" y1="140" x2="250" y2="110" />
+        <line x1="200" y1="140" x2="250" y2="110" className='arm2' />
       }
       {/* <!-- Legs --> */}
       {attempts < 2 &&
-        <line x1="200" y1="190" x2="240" y2="240" />
+        <line x1="200" y1="190" x2="240" y2="240" className='leg1' />
       }
       {attempts < 1 &&
-        <line x1="200" y1="190" x2="160" y2="240" />
+        <line x1="200" y1="190" x2="160" y2="240" className='leg2' />
       }
     </svg>
     <svg height="175" width="170" className="mob-fig-container">
       {/* <!-- Rod --> */}
-      <line x1="60" y1="20" x2="130" y2="20" />
+      <line x1="60" y1="20" x2="130" y2="20" className='line1' />
       <line x1="130" y1="20" x2="130" y2="50" />
       <line x1="60" y1="20" x2="60" y2="170" />
       <line x1="40" y1="170" x2="80" y2="170" />
 
       {/* <!-- Head --> */}
       {attempts < 6 &&
-        <circle cx="130" cy="65" r="15" />
+        <circle cx="130" cy="65" r="15" className='head' />
       }
       {/* <!-- Body --> */}
       {attempts < 5 &&
-        <line x1="130" y1="80" x2="130" y2="130" />
+        <line x1="130" y1="80" x2="130" y2="130" className='body' />
       }
       {/* <!-- Arms --> */}
       {attempts < 4 &&
-        <line x1="130" y1="100" x2="150" y2="85" />
+        <line x1="130" y1="100" x2="150" y2="85" className='arm1' />
       }
       {attempts < 3 &&
-        <line x1="130" y1="100" x2="110" y2="85" />
+        <line x1="130" y1="100" x2="110" y2="85" className='arm2' />
       }
       {/* <!-- Legs --> */}
       {attempts < 2 &&
-        <line x1="130" y1="130" x2="145" y2="150" />
+        <line x1="130" y1="130" x2="145" y2="150" className='leg1' />
       }
       {attempts < 1 &&
-        <line x1="130" y1="130" x2="115" y2="150" />
+        <line x1="130" y1="130" x2="115" y2="150" className='leg2' />
       }
     </svg>
     </div>
